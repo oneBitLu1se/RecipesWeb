cea60b0a59a4b090c1a0e1ca2400b8f49750ed34
Luise <luise.schulz@fh-erfurt.de>
2024-09-03 16:32:40 +0200

    old files

---
+++ src/main/java/com/lilase/recipesweb/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.lilase.recipesweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/Home")
+    public String home() {
+        return "Homepage";
+    }
+}
---
+++ src/test/java/com/lilase/recipesweb/RecipesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilase.recipesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
